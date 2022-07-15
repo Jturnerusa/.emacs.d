04f350f889db7e2c73e7be48797a5ba83b891dc7
John Turner <jturner.usa@gmail.com>
2022-07-14 22:12:27 -0400

    removed backup-before-save

--- load-local-lisp.el
+++ load-local-lisp.el
@@ -1,7 +1,4 @@
 (let ((local-lisp-directory (file-name-concat user-emacs-directory "lisp")))
-  (add-to-list 'load-path (file-name-concat local-lisp-directory "backup-before-save"))
   (add-to-list 'load-path (file-name-concat local-lisp-directory "man-wrapper")))
 
-(require 'backup-before-save)
 (require 'man-wrapper)
-  
