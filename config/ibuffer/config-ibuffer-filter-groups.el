602769dc8ccf163cd558ca29dd19c11ea0767ab9
John Turner <jturner.usa@gmail.com>
2022-10-04 22:43:35 -0400

    updated filter groups
    We changed the filter groups labels to start with a capital to keep
    the style consistent with the Default group. We also added a new Man
    filter group and a condition in the "Misc" group to exclude man pages.

--- config/ibuffer/config-ibuffer-filter-groups.el
+++ config/ibuffer/config-ibuffer-filter-groups.el
@@ -1,7 +1,9 @@
 (setq ibuffer-saved-filter-groups
       '(("default"
-         ("misc" (name . "^\\*.*$"))
-         ("dired" (mode . dired-mode)))))
+         ("Misc" (and (name . "^\\*.*$")
+                      (not (mode . Man-mode))))
+         ("Dired" (mode . dired-mode))
+         ("Man" (mode . Man-mode)))))
 
 (add-hook 'ibuffer-mode-hook (lambda () (ibuffer-switch-to-saved-filter-groups "default")))
 
