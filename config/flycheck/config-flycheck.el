8a028d54dd3e694e0497cea2ba3c34d02d37aa16
John Turner <jturner.usa@gmail.com>
2022-10-04 22:32:18 -0400

    removed display-buffer-alist update function
    Before we had a function that updated display-buffer-alist whenever
    the frame's size was changed, so that info windows like flymake or
    flycheck open would snap to either the bottom or right hand side of the
    frame depending on whether the window manager had the frame squeezed
    vertically (like when you have two windows side by side split
    vertically).
    
    Now instead of having that function run every time the frame resizes,
    we just make the 'side property a function that should get evaluated
    each time the property is read. The function returns either 'bottom or
    'right depending on the frame's current size in pixels.

--- config/flycheck/config-flycheck.el
+++ config/flycheck/config-flycheck.el
@@ -1,7 +1,13 @@
 (require 'flycheck)
 
+(load "config-flycheck-cargo-has-command-p-fix")
+
 (add-hook 'flycheck-error-list-mode-hook (lambda () (visual-line-mode 1)))
 
-(load "config-flycheck-cargo-has-command-p-fix")
+(add-to-list 'display-buffer-alist '("\\*Flycheck errors.*"
+                                     (display-buffer-in-side-window)
+                                     (side . (config-display-buffer-which-side))
+                                     (window-height . 15)
+                                     (window-width . 75)))
 
 (provide 'config-flycheck)
--- config/flymake/config-flymake.el
+++ config/flymake/config-flymake.el
@@ -2,4 +2,10 @@
 
 (load "config-flymake-hooks")
 
+(add-to-list 'display-buffer-alist '("\\*Flymake diagnostics.*"
+                                     (display-buffer-in-side-window)
+                                     (side . (config-display-buffer-which-side))
+                                     (window-height . 15)
+                                     (window-width . 75)))
+
 (provide 'config-flymake)
