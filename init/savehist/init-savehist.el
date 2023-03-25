1f80e2b5c5bc002172edc7a85a1c9f269bc44bb1
John Turner <jturner.usa@gmail.com>
2023-03-25 03:36:19 -0400

    never forgot command history

--- init/savehist/init-savehist.el
+++ init/savehist/init-savehist.el
@@ -1,9 +1,11 @@
 (require 'savehist)
 
+(defvar init-savehist-history-size 9999999999)
+
 (setq savehist-file (expand-file-name "savehist" user-emacs-directory)
       savehist-save-minibuffer-history t)
 
-(add-to-list 'savehist-additional-variables 'command-history)
+(add-to-list 'savehist-additional-variables `(command-history . ,init-savehist-history-size))
 
 (unless (file-exists-p savehist-file)
   (make-empty-file savehist-file))
