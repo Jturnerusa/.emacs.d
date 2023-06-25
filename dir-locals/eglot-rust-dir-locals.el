e166c4beaeddbf362e500ac20bd03c37a69a0ac0
John Turner <jturner.usa@gmail.com>
2023-06-24 21:39:40 -0400

    update eglot-rust-dir-locals.el

--- dir-locals/eglot-rust-dir-locals.el
+++ dir-locals/eglot-rust-dir-locals.el
@@ -1,16 +1,19 @@
 ((rust-ts-mode . ((eval . (progn
                             (require 'rust-mode)
-                            (eglot-ensure)
+                            (require 'eglot)
+                            (advice-add 'eglot-inlay-hints-mode :around 'ignore)
+                            (eglot-ensure)                            
+                            (company-mode 1)
                             (use-local-map (make-composed-keymap (list rust-mode-map rust-ts-mode-map)))
                             (add-hook 'before-save-hook rust-before-save-hook nil t)
                             (add-hook 'after-save-hook rust-after-save-hook nil t)
-                            (keymap-set (current-local-map) "C-c C-c C-c C-d" (lambda ()
-                                                                                (interactive)
-                                                                                (rust--compile
-                                                                                 "%s doc"
-                                                                                 rust-cargo-bin)))))
-                  (eglot-workspace-configuration . ((:rust-analyzer . (:checkOnSave
-                                                                       (:command "clippy")))))
+                            (keymap-set (current-local-map)
+                                        "C-c C-c C-d"
+                                        (lambda ()
+                                          (interactive)
+                                          (rust--compile
+                                            "env TMPDIR=${HOME}/tmp cargo test && cargo doc")))))
+                  (eglot-workspace-configuration . (:rust-analyzer  (:check
+                                                                     (:command "clippy"))))
                   (rust-format-on-save . t)               
-                  (rust-rustfmt-switches . ("--edition" "2021"))))
- (nil . ((rust-cargo-bin . "env TMPDIR=~/tmp cargo"))))
+                  (rust-rustfmt-switches . ("--edition" "2021")))))
