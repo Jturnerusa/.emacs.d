0a6d48010b25cdf5654035786a8db599cbff643f
John Turner <jturner.usa@gmail.com>
2023-09-12 00:38:46 -0400

    use best-side-window for flymake diagnostics buffers

--- packages/flymake.el
+++ packages/flymake.el
@@ -1,3 +1,7 @@
 (use-package flymake
   :hook
-  (flymake-diagnostics-buffer-mode . visual-line-mode))
+  (flymake-diagnostics-buffer-mode . visual-line-mode)
+  :config
+  (when (require 'best-side-window nil t)
+    (add-to-list 'display-buffer-alist '((major-mode . flymake-diagonstics-buffer-mode)
+                                         best-side-window-display-buffer-in-best-side-window))))
