982f716ff746b00e23868137f0069755e83b89c9
John Turner <jturner.usa@gmail.com>
2023-09-12 00:42:05 -0400

    add /root/bin to tramp-remote-path

--- packages/tramp.el
+++ packages/tramp.el
@@ -6,3 +6,4 @@
   (add-to-list 'tramp-connection-properties
                (list (regexp-quote (format "/sudo:root@%s:" system-name))
                      "session-timeout" (* 60 60)))
+  (add-to-list 'tramp-remote-path "/root/bin"))
