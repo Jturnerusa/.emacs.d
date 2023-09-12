42fe62da24704155e74528d738830492fe64e2a8
John Turner <jturner.usa@gmail.com>
2023-09-12 00:45:02 -0400

    update eglot-rust-dir-locals.el

--- dir-locals/eglot-rust-dir-locals.el
+++ dir-locals/eglot-rust-dir-locals.el
@@ -1,19 +1,10 @@
 ((rust-ts-mode . ((eval . (progn
-                            (require 'rust-mode)
                             (require 'eglot)
-                            (advice-add 'eglot-inlay-hints-mode :around 'ignore)
-                            (eglot-ensure)                            
-                            (company-mode 1)
-                            (use-local-map (make-composed-keymap (list rust-mode-map rust-ts-mode-map)))
-                            (add-hook 'before-save-hook rust-before-save-hook nil t)
-                            (add-hook 'after-save-hook rust-after-save-hook nil t)
-                            (keymap-set (current-local-map)
-                                        "C-c C-c C-d"
-                                        (lambda ()
-                                          (interactive)
-                                          (rust--compile
-                                            "env TMPDIR=${HOME}/tmp cargo test && cargo doc")))))
-                  (eglot-workspace-configuration . (:rust-analyzer  (:check
-                                                                     (:command "clippy"))))
-                  (rust-format-on-save . t)               
-                  (rust-rustfmt-switches . ("--edition" "2021")))))
+                            (eglot-ensure)
+                            (setq-local eglot-workspace-configuration '(:rust-analyzer  (:checkb
+                                                                                         (:command "clippy"))))
+                            (when (require 'company nil t)
+                              (company-mode 1))
+                            (when (require 'rust-mode nil t)
+                              (setq-local rust-format-on-save t
+                                          rust-rustfmt-switches  '("--edition" "2021"))))))))
