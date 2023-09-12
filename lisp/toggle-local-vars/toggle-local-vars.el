086b24b8502b41a719aaa0882decb6f49592664d
John Turner <jturner.usa@gmail.com>
2023-09-12 00:42:47 -0400

    add functions to toggle remote local variables

--- lisp/toggle-local-vars/toggle-local-vars.el
+++ lisp/toggle-local-vars/toggle-local-vars.el
@@ -8,4 +8,15 @@
   (setq enable-local-variables nil)
   (message "disabled local variables"))
 
+(defun toggle-remote-local-vars-on ()
+  (interactive)
+  (setq enable-remote-dir-locals t
+        enable-local-variables t)
+  (message "enabled local variables"))
+
+(defun toggle-remote-local-vars-off ()
+  (interactive)
+  (setq enable-remote-dir-locals nil)
+  (message "disabled local variables"))
+
 (provide 'toggle-local-vars)
