4f2985ce0273fab2e7d555430e2dabb4095fb9cd
John Turner <jturner.usa@gmail.com>
2023-09-25 05:49:24 -0400

    set license-boilerplate-copyright-author variable

--- lisp/packages.el
+++ lisp/packages.el
@@ -27,7 +27,9 @@
   :load-path (lambda () (locate-user-emacs-file "lisp/toggle-local-vars")))
 
 (use-package license-boilerplate
-  :load-path (lambda () (locate-user-emacs-file "lisp/license-boilerplate")))
+  :load-path (lambda () (locate-user-emacs-file "lisp/license-boilerplate"))
+  :custom
+  (license-boilerplate-copyright-author "John Turner"))
 
 (use-package best-side-window
   :load-path (lambda () (locate-user-emacs-file "lisp/best-side-window")))
