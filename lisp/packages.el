09fe05176537f871aeb1d65237fe13cd4dc0c0d5
John Turner <jturner.usa@gmail.com>
2023-09-25 05:50:16 -0400

    create lisp library clang-format

--- lisp/packages.el
+++ lisp/packages.el
@@ -33,3 +33,6 @@
 
 (use-package best-side-window
   :load-path (lambda () (locate-user-emacs-file "lisp/best-side-window")))
+
+(use-package clang-format
+  :load-path (lambda () (locate-user-emacs-file "lisp/clang-format")))
