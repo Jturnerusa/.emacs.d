0b1d35fa572c5627cf3ee4dd7024f7536c0e258a
John Turner <jturner.usa@gmail.com>
2023-09-25 04:59:16 -0400

    typo

--- packages/flymake.el
+++ packages/flymake.el
@@ -3,5 +3,6 @@
   (flymake-diagnostics-buffer-mode . visual-line-mode)
   :config
   (when (require 'best-side-window nil t)
-    (add-to-list 'display-buffer-alist '((major-mode . flymake-diagonstics-buffer-mode)
+    (add-to-list 'display-buffer-alist '((major-mode . flymake-diagnostics-buffer-mode)
                                          best-side-window-display-buffer-in-best-side-window))))
+
