46141609acc161123edecc3e8f7b32da87b5f3a1
John Turner <jturner.usa@gmail.com>
2023-10-23 18:44:49 -0400

    fix whitespace insertion

--- lisp/license-boilerplate/license-boilerplate.el
+++ lisp/license-boilerplate/license-boilerplate.el
@@ -14,7 +14,7 @@
       (when-let ((author license-boilerplate-copyright-author))
         (insert (format " %s" author)))
       (insert "\n")
-      (insert "\
+      (insert "
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
