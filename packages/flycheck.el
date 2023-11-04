72c0d999b63827eca4b0ac03bc99101b5b9b81de
John Turner <jturner.usa@gmail.com>
2023-11-04 18:17:06 -0400

    setup flycheck to use best-side-window for displaying errors

--- packages/flycheck.el
+++ packages/flycheck.el
@@ -1,3 +1,7 @@
 (use-package flycheck
   :hook
-  (flycheck-error-list-mode . visual-line-mode))
+  (flycheck-error-list-mode . visual-line-mode)
+  :config
+  (when (require 'best-side-window nil t)
+    (add-to-list 'display-buffer-alist '((major-mode . flycheck-error-list-mode)
+                                         best-side-window-display-buffer-in-best-side-window))))
