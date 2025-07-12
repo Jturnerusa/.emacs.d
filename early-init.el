cd7dd6322445cd3126c4e5c736f583b17b0e2337
John Turner <jturner.usa@gmail.com>
2025-07-12 13:12:31 -0400

    add native-compile-target-directory to native-comp-eln-load-path

--- early-init.el
+++ early-init.el
@@ -1,2 +1,4 @@
 (setq native-comp-async-jobs-number (string-to-number (shell-command-to-string "nproc"))
-      native-compile-target-directory (file-name-concat (getenv "HOME") ".cache/emacs-native-comp"))
+      native-compile-target-directory (file-name-concat (getenv "HOME") ".cache/emacs-native-comp")
+      native-comp-eln-load-path (cons native-compile-target-directory native-comp-eln-load-path)
+      gc-cons-threshold (* 64 (expt 1024 2)))
