19cce3b2f738afad2cab4a27eace57d1495ee790
John Turner <jturner.usa@gmail.com>
2025-08-22 22:40:05 -0400

    dont disable arrow keys (useful for emacs on phone)

--- init.el
+++ init.el
@@ -6,14 +6,6 @@
 (require 'edebug)
 (require 'xdg)
 
-(global-unset-key (kbd "<left>"))
-(global-unset-key (kbd "<right>"))
-(global-unset-key (kbd "<up>"))
-(global-unset-key (kbd "<down>"))
-(global-unset-key (kbd "<C-left>"))
-(global-unset-key (kbd "<C-right>"))
-(global-unset-key (kbd "<C-up>"))
-(global-unset-key (kbd "<C-down>"))
 (global-set-key   (kbd "C-x k")     'kill-current-buffer)
 (global-set-key   (kbd "C-x C-b")   'ibuffer)
 
