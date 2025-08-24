2a17e2b71fdb4b59847d233f22fab0ba60b03649
John Turner <jturner.usa@gmail.com>
2025-08-23 20:00:07 -0400

    use -Z flag for "ls" for dired to show selinux labels

--- packages/dired.el
+++ packages/dired.el
@@ -1,4 +1,4 @@
 (use-package dired
   :custom
-  (dired-listing-switches "-ahl --group-directories-first")
+  (dired-listing-switches "-ahlZ --group-directories-first")
   (dired-kill-when-opening-new-dired-buffer t))
