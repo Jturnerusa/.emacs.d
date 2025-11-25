3e36a5642184963db4fa1dd2f283099059177045
John Turner <jturner.usa@gmail.com>
2025-11-24 22:50:21 -0500

    customize flycheck-python-project-files

--- packages/flycheck.el
+++ packages/flycheck.el
@@ -3,6 +3,7 @@
   (flycheck-error-list-mode . visual-line-mode)
   :custom
   (flycheck-python-mypy-cache-dir (file-name-concat (getenv "HOME") ".cache" "mypy"))
+  (flycheck-python-project-files '("meson.build" "pyproject.toml" ".mypy.ini"))
   :config
   (when (require 'best-side-window nil t)
     (add-to-list 'display-buffer-alist '((major-mode . flycheck-error-list-mode)
