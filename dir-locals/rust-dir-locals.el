5fc02b8d5faacc0e5bfae7d49f0d977f6d72c9ff
John Turner <jturner.usa@gmail.com>
2025-11-24 22:50:03 -0500

    overhaul rust dir-locals

--- dir-locals/rust-dir-locals.el
+++ dir-locals/rust-dir-locals.el
@@ -1,4 +1,36 @@
-((rust-ts-mode . ((eglot-workspace-configuration . (:rust-analyzer (:check (:command "clippy"))))
-                  (fmt-executable . "rustfmt")
+((rust-mode . ((fmt-executable . "rustfmt")
+               (fmt-args . ("--edition=2024"))
+               (rust-cargo-default-arguments . "-r")
+               (eval . (eglot-ensure))
+               (eval . (company-mode 1))
+               (eval . (add-to-list
+                        'eglot-server-programs
+                        '((rust-ts-mode rust-mode) .
+                          ("rust-analyzer"
+                           :initializationOptions
+                           (:check
+                            (:command "clippy"))))))
+               (eval . (add-hook 'before-save-hook 'fmt-current-buffer nil t))
+               (eval . (add-hook 'eglot-managed-mode-hook (lambda ()
+                                                            (eglot-inlay-hints-mode -1))
+                                 nil t))))
+ (rust-ts-mode . ((fmt-executable . "rustfmt")
+                  (fmt-args . ("--edition=2024"))
+                  (rust-cargo-default-arguments . "-r")
                   (eval . (eglot-ensure))
-                  (eval . (add-hook 'before-save-hook 'fmt-current-buffer nil t)))))
+                  (eval . (company-mode 1))
+                  (eval . (add-to-list
+                           'eglot-server-programs
+                           '((rust-ts-mode rust-mode) .
+                             ("rust-analyzer"
+                              :initializationOptions
+                              (:check
+                               (:command "clippy"))))))                  
+                  (eval . (add-hook 'before-save-hook 'fmt-current-buffer nil t))
+                  (eval . (add-hook 'eglot-managed-mode-hook (lambda ()
+                                                               (eglot-inlay-hints-mode -1))
+                                    nil t))))
+ (meson-mode
+  . ((fmt-executable . "meson")
+     (fmt-args . ("format" "-"))
+     (eval . (add-hook 'before-save-hook 'fmt-current-buffer nil t)))))
