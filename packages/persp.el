3fc47c1e8b7014777b77d34dbb9ecffc6c06a58e
John Turner <jturner.usa@gmail.com>
2025-12-21 23:50:59 -0500

    set persp-save-dir

--- packages/persp.el
+++ packages/persp.el
@@ -1,3 +1,6 @@
 (use-package persp-mode
   :custom
-  (persp-add-buffer-on-after-change-major-mode  t))
+  (persp-add-buffer-on-after-change-major-mode t)
+  (persp-save-dir (file-name-concat
+                   (or (getenv "XDG_STATE_DIR") (file-name-concat (getenv "HOME") ".local/state"))
+                   "emacs/persp")))
