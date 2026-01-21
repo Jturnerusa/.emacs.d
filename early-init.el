f46c22c4b01ba9caaa61fc6fca19ee3a7d8fd26b
John Turner <jturner.usa@gmail.com>
2026-01-21 14:15:29 -0500

    only set native-comp variables when native comp is available

--- early-init.el
+++ early-init.el
@@ -1,4 +1,6 @@
-(setq native-comp-async-jobs-number (string-to-number (shell-command-to-string "nproc"))
-      native-compile-target-directory (file-name-concat (getenv "HOME") ".cache/emacs-native-comp")
-      native-comp-eln-load-path (cons native-compile-target-directory native-comp-eln-load-path)
-      gc-cons-threshold (* 128 (expt 1024 2)))
+(when (native-comp-available-p)
+  (setq native-comp-async-jobs-number (string-to-number (shell-command-to-string "nproc"))
+	native-compile-target-directory (file-name-concat (getenv "HOME") ".cache/emacs-native-comp")
+	native-comp-eln-load-path (cons native-compile-target-directory native-comp-eln-load-path)))
+  
+(setq gc-cons-threshold (* 128 (expt 1024 2)))
