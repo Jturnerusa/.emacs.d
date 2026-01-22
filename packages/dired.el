ab5b3328a6b809cfa43d07ddf3efc11d522fc101
John Turner <jturner.usa@gmail.com>
2026-01-22 15:48:26 -0500

    fix another typo

--- packages/dired.el
+++ packages/dired.el
@@ -3,7 +3,7 @@
   (setq dired-listing-switches
         (if (and (file-directory-p "/sys/fs/selinux")
                  (not (getenv "TERMUX_VERSION")))
-            "-ahlZ -+grouo-directories-first"
+            "-ahlZ --grouo-directories-first"
           "-ahl --group-directories-first"))
   :custom
   (dired-kill-when-opening-new-dired-buffer t))
