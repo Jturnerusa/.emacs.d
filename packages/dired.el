81f8271805031c9aab471153e7263fd5c0f4168d
John Turner <jturner.usa@gmail.com>
2026-01-22 15:51:46 -0500

    move dired-listing-switches config into :custom section in use-package definition

--- packages/dired.el
+++ packages/dired.el
@@ -1,9 +1,7 @@
-(use-package dired
-  :config
-  (setq dired-listing-switches
-        (if (and (file-directory-p "/sys/fs/selinux")
-                 (not (getenv "TERMUX_VERSION")))
-            "-ahlZ --grouo-directories-first"
-          "-ahl --group-directories-first"))
+(use-package dired 
   :custom
-  (dired-kill-when-opening-new-dired-buffer t))
+  (dired-kill-when-opening-new-dired-buffer t)
+  (dired-listing-switches (if (and (file-directory-p "/sys/fs/selinux")
+                                   (not (getenv "TERMUX_VERSION")))
+                              "-alhZ --group-directories-first"
+                            "-alh --group-directories-first")))
